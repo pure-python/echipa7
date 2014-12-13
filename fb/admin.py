c05565cbcb9b16900473ac3bbe21a3bd5eab8ab9
Vitalie Maldur <maldur.vitalik@gmail.com>
2014-12-13 17:49:55 +0200

    fix

--- fb/admin.py
+++ fb/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from fb.models import UserPost, UserProfile
+from fb.models import UserPost, UserProfile, Image, Album
 
 
 admin.site.register(UserPost)
 admin.site.register(UserProfile)
+admin.site.register(Image)
+admin.site.register(Album)
+
+
